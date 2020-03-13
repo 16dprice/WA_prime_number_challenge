b2e7df7d9bc550b1d0667c546654945dff84fb06
DJ <donald.price077@topper.wku.edu>
2020-03-13 15:45:14 -0500

    storing prime and composite info in local storage

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ function checkPrime() {
     let num = document.getElementById('prime-input').value;
 
     if(isPrime(num)) {
+        storePrime(num);
         document.getElementById('yes').style.display = 'block';
         document.getElementById('no').style.display = 'none';
     } else {
+        storeComposite(num);
         document.getElementById('no').style.display = 'block';
         document.getElementById('yes').style.display = 'none';
     }
@@ -20,4 +22,23 @@ function isPrime(num) {
         if(num % i === 0) return false;
     }
     return true;
+}
+
+function storePrime(num) {
+    let existingPrimes = JSON.parse(localStorage.getItem("primes"));
+
+    if(existingPrimes === null) existingPrimes = [];
+    if(!existingPrimes.includes(num)) existingPrimes.push(num);
+
+    localStorage.setItem("primes", JSON.stringify(existingPrimes));
+
+}
+
+function storeComposite(num) {
+    let existingComposites = JSON.parse(localStorage.getItem("composites"));
+
+    if(existingComposites === null) existingComposites = [];
+    if(!existingComposites.includes(num)) existingComposites.push(num);
+
+    localStorage.setItem("composites", JSON.stringify(existingComposites));
 }

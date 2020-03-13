0e96b39de97059a5ec15b7983034e32263766db0
DJ <donald.price077@topper.wku.edu>
2020-03-13 15:38:59 -0500

    basic styling, prime checking, and responding

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-function checkPrime(button) {
+function checkPrime() {
 
+    let num = document.getElementById('prime-input').value;
 
+    if(isPrime(num)) {
+        document.getElementById('yes').style.display = 'block';
+        document.getElementById('no').style.display = 'none';
+    } else {
+        document.getElementById('no').style.display = 'block';
+        document.getElementById('yes').style.display = 'none';
+    }
 
+}
+
+function isPrime(num) {
+
+    let sqrtNum = Math.sqrt(num);
+
+    for(let i = 2; i <= sqrtNum; i++) {
+        if(num % i === 0) return false;
+    }
+    return true;
 }
